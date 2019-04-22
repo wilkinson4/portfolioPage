f4646bbaace4f24ddcd68bad3dc6e30f947fefff
wfw40869 <wfw40869@gmail.com>
2019-04-22 17:46:27 +0000

    Refactored sendMail function into middleware to make app.js more readable. Working on authorization of this app for contact form use.

--- middleware/index.js
+++ middleware/index.js
@@ -1,25 +1,33 @@
 // all the middleare goes here
 require('dotenv').config();
 
-let nodemailer = require('nodemailer'),
-    middlewareObj = {};
+const nodemailer = require('nodemailer'),
+      smtpTransport = require('nodemailer-smtp-transport'),
+      middlewareObj = {};
+      
+
     
 middlewareObj.sendMail = function(req, res, next) {
-    let mailOpts, smtpTrans;
-    smtpTrans = nodemailer.createTransport({
-        service: 'gmail',
+    let mailOpts, transporter;
+    transporter = nodemailer.createTransport(smtpTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
+            type: 'gmail',
             user: process.env.USER,
-            pass: process.env.PASSWORD
+            clientID: process.env.CLIENTID,
+            clientSecret: process.env.CLIENTSECRET,
+            refreshToken: process.env.REFRESHTOKEN,
         }
-    });
+    }));
     mailOpts = {
         from: req.body.email,
         to: process.env.USER,
         subject: "New Message From My Portfolio Site",
         text: `${req.body.name} says: ${req.body.message} \n (${req.body.email})`
     };
-    smtpTrans.sendMail(mailOpts, function(error, response) {
+    transporter.sendMail(mailOpts, function(error, response) {
         if (error) {
             console.log(error);
             req.flash("error", "Something went wrong. Please try again.");
