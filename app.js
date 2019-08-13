f85aa9d6b7ba3c112019c238b99a2fbb1970857d
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-08-12 23:22:20 -0500

    Added Google reCaptcha API to limit spam messages.

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require('express'),
     app = express(),
     serveStatic = require('serve-static'),
     bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
     flash = require('connect-flash'),
-    middleware = require('./middleware');
+    ReCAPTCHA = require('node-grecaptcha-verify').ReCAPTCHA,
+    middleware = require('./middleware')
 
 
 
 app.use(flash());
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(serveStatic('public/'));
@@ -32,15 +35,32 @@ app.use((req, res, next) => {
 
 //INDEX ROUTE
 app.get("/", (req, res) => {
-    res.render("home");
-});
+    res.render("home")
+})
 
 // POST ROUTE from contact form
 app.post("/", (req, res) => {
-    middleware.sendMail(req,res);
+    const reCaptcha = new ReCAPTCHA(process.env.CAPTCHA_SECRET, 0.5)
+    const getVerificationResult = async (reCaptcha) => {
+        return await reCaptcha.verify(req.cookies.token)
+    }
+    getVerificationResult(reCaptcha)
+        .then(verificationResult => {
+            // console.log(reCaptcha)
+            // console.log(verificationResult)
+            if (verificationResult.isHuman) {
+                // reCAPTCHA response was accepted!
+                middleware.sendMail(req, res)
+            } else {
+                // reCAPTCHA token denied.
+                req.flash("error", "Something went wrong. Please try again.");
+                res.redirect("/#contact");
+            }
+
+        })
 });
 
 
 app.listen(process.env.PORT, process.env.IP, () => {
-    console.log("The Server Has Started!");
+    console.log("The Server Has Started!")
 });
